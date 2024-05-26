3f903eb4b323384e1c2838b44765fc81c95e4fca
MariaWaBot <167265485+MariaWaBot@users.noreply.github.com>
2024-05-26 14:25:02 +0530

    Update getConfig.js

--- src/getConfig.js
+++ src/getConfig.js
@@ -1,13 +1,13 @@
 import 'dotenv/config'
 const getConfig = () => {
     return {
-        name: process.env.NAME || 'MARIA-MD,
-        session: process.env.SESSION || 'session',
-        prefix: process.env.PREFIX || '.',
+        name: process.env.NAME || 'MARIA-MD',
+        session: process.env.SESSION || 'Ak',
+        prefix: process.env.PREFIX || '-',
         port: process.env.PORT || 3000,
         imgbb: process.env.IMGBB,
-        chatboturi: process.env.BRAINSHOP || '',
-        mongo: process.env.MONGO || '',
+        chatboturi: process.env.BRAINSHOP || 'http://api.brainshop.ai/get?bid=165801&key=1ftAuFL7Fhj21Fyp&uid=[uid]&msg=[msg]',
+        mongo: process.env.MONGO || 'mongodb+srv://mongodb:y3tAo2XWyXXbdGXF@cluster0.riheec9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
         mods: process.env.MODS
             ? process.env.MODS.split(',').map((id) => {
                   if (id.endsWith('@s.whatsapp.net')) return id
