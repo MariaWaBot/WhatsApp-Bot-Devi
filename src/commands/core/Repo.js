c2c55587b0265267f225d51df4d32b6345c714ef
MariaWaBot <167265485+MariaWaBot@users.noreply.github.com>
2024-05-26 11:43:13 +0530

    Update Repo.js

--- src/commands/core/Repo.js
+++ src/commands/core/Repo.js
@@ -15,10 +15,10 @@ export default class Command extends BaseCommand {
 
     exec = async (M) => {
         const { open_issues_count, forks_count, stargazers_count, updated_at } = await this.client.util.fetch(
-            `https://api.github.com/repos/Debanjan-San/WhatsApp-Bot-Devi`
+            `https://api.github.com/repos/AYUSH-PANDEY023/Maria-MD`
         )
         return void (await M.replyRaw({
-            text: '*https://github.com/Debanjan-San/WhatsApp-Bot-Devi.git*',
+            text: '*https://github.com/AYUSH-PANDEY023/Maria-MD.git*',
             contextInfo: {
                 externalAdReply: {
                     title: `Updated at ${updated_at}`,
@@ -26,7 +26,7 @@ export default class Command extends BaseCommand {
                     thumbnail: await this.client.util.fetchBuffer('https://i.imgur.com/22WppSh.jpg'),
                     mediaType: 1,
                     mediaUrl: '',
-                    sourceUrl: 'https://github.com/Debanjan-San/WhatsApp-Bot-Devi.git',
+                    sourceUrl: 'https://github.com/AYUSH-PANDEY023/Maria-MD.git',
                     ShowAdAttribution: true
                 }
             }
